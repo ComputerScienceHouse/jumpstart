ed70faadab8318912aa639497569ab266cce9313
Weather <weathergod3218@gmail.com>
2026-03-04 03:13:33 -0500

    Started ICalendar impelementation

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM ghcr.io/astral-sh/uv:python3.14-alpine
 COPY src /jumpstart
 WORKDIR /jumpstart
 
+RUN addgroup -g 2000 jumpgroup
+RUN adduser -S -u 1001 -G jumpgroup jumpstart
+
 RUN uv pip install --no-cache-dir -r requirements.txt --system
 
 CMD ["echo", ":)"]
--- src/core/google.py
+++ src/core/google.py
@@ -1,11 +1,46 @@
+# Use googleapiclient instead, more modern!
+from googleapiclient.discovery import build
+
+from datetime import datetime
+import requests
+import recurring_ical_events
+from icalendar import Calendar
+import logging
 '''
-    Eli Mares
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SERVICE_ACCOUNT_FILE = "path/to/key.json"
+
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=SCOPES,
+)
+
+service = build("sheets", "v4", credentials=creds)
 '''
 
-'''
+#rti648k5hv7j3ae3a3rum8potk@group.calendar.google.com
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'] # The URL to google calender
+CALENDAR_URL = "https://calendar.google.com/calendar/embed?src=rti648k5hv7j3ae3a3rum8potk%40group.calendar.google.com&ctz=America%2FNew_York"
+calendar_service = build("calendar","v3")
+
+OUTLOOK_DAYS = 7 #The amount of days to go through
+
+def format_events(events : list):
+    return
+
+def get_future_events():
+    response = requests.get(CALENDAR_URL)
+    cal = Calendar.from_ical(response.content)
+
+    
+    current_day = datetime.now()
+    recurring_ical_events.of(cal).between(datetime.now())
 def calendar():
-    # Call the Calendar API
-    now = datetime.now(timezone.utc or pytz.utc)
+ # Call the Calendar API
+    now = datetime.now()
     events_result = calendar_service.events().list(
         calendarId='rti648k5hv7j3ae3a3rum8potk@group.calendar.google.com',
         timeMin=now.isoformat(),
@@ -45,4 +80,4 @@ def calendar():
     event_list = {'data': final_events}
     return jsonify(event_list)
 
-'''
+ 
